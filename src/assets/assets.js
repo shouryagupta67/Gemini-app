f76520ae83b8f4e807f9d8d96a52715a1e922eda
Shourya Gupta <shouryasahu08@gmail.com>
2024-11-28 09:15:00 +0530

    Gemeni clone

---
+++ src/assets/assets.js
@@ -0,0 +1,35 @@
+import menu_icon from './menu_icon.png'
+import plus_icon from './plus_icon.png'
+import history_icon from './history_icon.png'
+import bulb_icon from './bulb_icon.png'
+import code_icon from './code_icon.png'
+import compass_icon from './compass_icon.png'
+import gallery_icon from './gallery_icon.png'
+import gallery_icon2 from './gallery_icon2.png'
+import gemini_icon from './gemini_icon.png'
+import message_icon from './message_icon.png'
+import mic_icon from './mic_icon.png'
+import question_icon from './question_icon.png'
+import send_icon from './send_icon.png'
+import setting_icon from './setting_icon.png'
+import user_icon from './user_icon.png'
+import youtube_icon from './youtube_icon.png'
+
+export const assets ={
+    history_icon,
+    menu_icon,
+    plus_icon,
+    bulb_icon,
+    code_icon,
+    compass_icon,
+    gallery_icon,
+    gallery_icon2,
+    gemini_icon,
+    message_icon,
+    mic_icon,
+    question_icon,
+    send_icon,
+    setting_icon,
+    user_icon,
+    youtube_icon,
+}
