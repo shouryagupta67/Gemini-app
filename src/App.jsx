7f3856d9010a12566be5e49af0eb226d694dbd4b
Shourya Gupta <shouryasahu08@gmail.com>
2024-11-28 10:25:41 +0530

    Add context.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
+import Sidebar from './components/sidebar/Sidebar'
 import Main from './components/Main/Main'
 
 function App() {
